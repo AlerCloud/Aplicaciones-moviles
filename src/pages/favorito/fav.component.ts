f358bd30618d84d46d8ab742f912aa4929a837cc
AlerCloud <al.uriber@duocuc.cl>
2021-12-03 02:09:52 -0300

    prueba de mapa

--- src/pages/favorito/fav.component.ts
+++ src/pages/favorito/fav.component.ts
@@ -1,18 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { AlertController } from "@ionic/angular";
-import { __await } from "tslib";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Marker } from 'src/app/dato/interfaz/IMarker'
+import { LoadingController, IonSlides } from "@ionic/angular";
 
 
 declare var google;
-
-interface Marker {
-  position: {
-    lat:number,
-    lng:number,
-  };
-  title: string;
-}
-
 
 
  @Component({
@@ -22,53 +13,93 @@ interface Marker {
  })
 
  export class FavComponent implements OnInit {
-   map = null;
 
-    constructor(){}
+  @ViewChild(IonSlides) slides: IonSlides;
 
 
-    ngOnInit(){
-      this.loadMap();
+    mapRef = null;
+    infoWindowsRef = null;
+
+    //Lugares guardados para que se muenstren en el telefono
+
+    markers: Marker[] = [
+      {
+        lat: -33.745719,
+        lng:  -70.746316,
+        title: 'casita',
+        image: 'https://lh5.googleusercontent.com/p/AF1QipOCgzq_0DYB9AxD-ItTG01x2csLsSfWsawBCypc=w408-h306-k-no',
+        text: 'Anialt ratione placeat necessitatibus quisquam molestiae obcaecati laudantium?'
+      },
+      {
+        lat: -33.744363,
+        lng: -70.751395,
+        title: 'j3',
+        image: 'https://lh5.googleusercontent.com/p/AF1QipMGZeu88O8uZvFOX9PKug7gz-VRhhiXQ78hAFZU=w408-h306-k-no',
+        text: 'Animi it ratione placeat necessitatibus quisquam molestiae obcaecati laudantium?'
+      },
+      {
+        lat: -33.733913,
+        lng:  -70.750108,
+        title: 'Supermercado',
+        image: 'https://lh5.googleusercontent.com/p/AF1QipMGZeu88O8uZvFOX9PKug7gz-VRhhiXQ78hAFZU=w408-h306-k-no',
+        text: 'olestiae obcaecati laudantium?'
+      },
+    ]
+
+    constructor(
+      private loadingCtrl: LoadingController
+    ){
+      this.infoWindowsRef = new google.maps.InfoWindow();
     }
 
-  loadMap(){
-    const mapEle: HTMLElement = document.getElementById('map');
-    const myLatLng = {lat: -33.746113, lng: -70.745763};
-    this.map = new google.maps.Map(mapEle, {
-      center: myLatLng,
-      zoom: 12
-    });
 
-    google.maps.event.addListenerOnce(this.map, 'idle', () => {
-      
-      mapEle.classList.add('show-map');
-      const marker= {
-        position: {
-          lat:-33.746113 ,
-          lng:-70.745763
-        },
-        title :'punto'
-      };
-      this.addMarker(marker);
-      
-    });
-  } 
-  addMarker(marker: Marker){
-    return new google.maps.Marker({
-      position: marker.position,
-      map: this.map,
-      title: marker.title
-    });
+
+    ngOnInit() {
+      this.loadMap();
+    }
+    //carga de mapa
+  async  loadMap(){
+      const loading = await this.loadingCtrl.create();
+      await loading.present();
+      const mapEle: HTMLElement = document.getElementById('map');
+      const marker = this.markers[0];
+      this.mapRef = new google.maps.Map(mapEle, {
+        center: {lat: marker.lat, lng: marker.lng },
+        zoom: 16
+      });
+  
+      google.maps.event.addListenerOnce(this.mapRef, 'idle', () => {
+        loading.dismiss();
+        this.loadMarkers();
+      });
+    }   
+    //genera el marcador
+    private addMaker(itemMarker: Marker) {
+      const marker = new google.maps.Marker({
+        position: { lat: itemMarker.lat, lng: itemMarker.lng },
+        map: this.mapRef,
+        title: itemMarker.title
+      });
+      return marker;
+    }
+    //carga el marcador
+    private loadMarkers(){
+      this.markers.forEach(marker => {
+        const markerObj = this.addMaker(marker);
+        marker.markerObj = markerObj;
+      });
+    }
+    //muestra el lugar del punto
+  async onSlideDidChange(){
+    const currentSlide = await this.slides.getActiveIndex();
+    const marker = this.markers[currentSlide];
+    this.mapRef.panTo({lat: marker.lat, lng: marker.lng});
+  
+    //genera el mensaje
+  const markerObj = marker.markerObj;
+  this.infoWindowsRef.setContent(marker.title);
+  this.infoWindowsRef.open(this.mapRef, markerObj);
   }
+} 
 
-
-
-
-
-
-
-
-
-
- }
      
